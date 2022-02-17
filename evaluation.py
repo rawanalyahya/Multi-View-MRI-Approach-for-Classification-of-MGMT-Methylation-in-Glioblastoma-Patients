825fda48711d06f78a0ed2270eb74531bc160132
rawanalyahya <87375561+rawanalyahya@users.noreply.github.com>
2022-02-17 15:17:38 +0300

    Add files via upload

--- evaluation.py
+++ evaluation.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import label_binarize
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc,roc_auc_score
 from sklearn import preprocessing
 
 
-
+#evaluation metrics for a three-inputs experiment
 def get_confusion_matrix_AUC_three_inputs(model, dataloader, nb_classes=2):
     truth = []
     predections = []
@@ -46,34 +46,6 @@ def get_confusion_matrix_AUC_three_inputs(model, dataloader, nb_classes=2):
 
     print(confusion_matrix)
 
-
-    y = label_binarize(truth, classes=[0,1, 2])
-
-    roc_auc = dict()
-    tpr = dict()
-    fpr = dict()
-    thresholds = dict()
-    for i in range(nb_classes-1):
-
-        fpr[i], tpr[i], thresholds[i] = roc_curve(y[:,i], predections[:,i], drop_intermediate=False)
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-  
-
-    # for i in range(nb_classes-1):
-    #     plt.figure()
-    #     plt.plot(fpr[i], tpr[i], label='ROC curve (area = %0.5f)' % roc_auc[i])
-    #     plt.plot([0, 1], [0, 1], 'k--')
-    #     plt.xlim([0.0, 1.0])
-    #     plt.ylim([0.0, 1.05])
-    #     plt.xlabel('False Positive Rate')
-    #     plt.ylabel('True Positive Rate')
-    #     plt.title('Class ' + str(i))
-    #     plt.legend(loc="lower right")
-    #     plt.show()
-
-    return roc_auc[0]
-
 def get_f1_score_precision_recall_three_inputs(model, dataloader):
     truth = []
     predections = []
@@ -92,13 +64,13 @@ def get_f1_score_precision_recall_three_inputs(model, dataloader):
             outputs = model.to("cuda")(input_1,input_2,input_3)
         
             m = nn.Softmax()
-            softmax_output = m(outputs)       
+            #softmax_output = m(outputs)       
             #outputs = torch.column_stack((softmax_output[:,0] > 0.110333, softmax_output[:,1])).float()
 
             if len(predections) == 0:
-                predections = softmax_output.cpu().detach().numpy()
+                predections = outputs.cpu().detach().numpy()
             else:
-                predections= np.concatenate((predections, softmax_output.cpu().detach().numpy()), axis=0)
+                predections= np.concatenate((predections, outputs.cpu().detach().numpy()), axis=0)
             
             for k in range(labels.shape[0]):
                 truth.append(labels[k].item())
@@ -130,7 +102,67 @@ def get_f1_score_precision_recall_three_inputs(model, dataloader):
 
     return f1_score, recall, precision
 
-#six inputs
+
+
+
+
+#evaluation metrics for a six-inputs experiment
+def get_f1_score_precision_recall_six_inputs(model, dataloader):
+    truth = []
+    predections = []
+    with torch.no_grad():
+        for i, batch in enumerate(dataloader):
+            model.eval()
+            input_1 = batch["image"][0].to("cuda", dtype=torch.float)
+            input_2 = batch["image"][1].to("cuda", dtype=torch.float)
+            input_3 = batch["image"][2].to("cuda", dtype=torch.float)
+            input_4 = batch["image"][3].to("cuda", dtype=torch.float)
+            input_5 = batch["image"][4].to("cuda", dtype=torch.float)
+            input_6 = batch["image"][5].to("cuda", dtype=torch.float)
+        
+        
+            labels = batch["label"]
+            labels = np.asarray(labels)
+            labels = torch.from_numpy(labels.astype('long')).to("cuda")
+
+            outputs = model.to("cuda")(input_1, input_2, input_3, input_4, input_5, input_6)
+        
+            m = nn.Softmax()
+            softmax_output = m(outputs)       
+            #outputs = torch.column_stack((softmax_output[:,0] > 0.110333, softmax_output[:,1])).float()
+
+            if len(predections) == 0:
+                predections = softmax_output.cpu().detach().numpy()
+            else:
+                predections= np.concatenate((predections, softmax_output.cpu().detach().numpy()), axis=0)
+            
+            for k in range(labels.shape[0]):
+                truth.append(labels[k].item())
+
+    predicted_classes = np.argmax(predections, axis=1)
+    tp = 0
+    fn = 0
+    fp = 0
+    for i in range(len(truth)):
+
+        if (truth[i]==1 and predicted_classes[i] ==1):
+
+            tp+=1
+
+        elif (truth[i]==1 and predicted_classes[i] ==0):
+
+            fn+=1
+
+        elif (truth[i]==0 and predicted_classes[i] ==1):
+
+            fp+=1
+
+    precision = tp / (tp+fp)
+    recall = tp / (tp+fn)
+    f1_score = 2 * precision * recall / (precision + recall)
+
+    print("f1 score = {}, precision ={}, recall = {}".format(f1_score, precision, recall))
+
 def get_confusion_matrix_AUC_six_inputs(model, dataloader, nb_classes=2):
     truth = []
     predections = []
@@ -172,7 +204,7 @@ def get_confusion_matrix_AUC_six_inputs(model, dataloader, nb_classes=2):
     print(confusion_matrix)
 
 
-    y = label_binarize(truth, classes=[0,1,2])
+    y = label_binarize(truth, classes=[0,1])
 
 
     pr = dict()
@@ -201,6 +233,7 @@ def get_confusion_matrix_AUC_six_inputs(model, dataloader, nb_classes=2):
         plt.show()
 
 
+#evaluation metrics for a two-inputs experiment
 def get_confusion_matrix_AUC_two_inputs(model, dataloader, input1, input2,  nb_classes=2):
     truth = []
     predections = []
@@ -238,7 +271,7 @@ def get_confusion_matrix_AUC_two_inputs(model, dataloader, input1, input2,  nb_c
     print(confusion_matrix)
 
 
-    y = label_binarize(truth, classes=[0,1,2])
+    y = label_binarize(truth, classes=[0,1])
 
 
     pr = dict()
@@ -321,61 +354,8 @@ def get_f1_score_precision_recall_two_inputs(model, dataloader, input1, input2):
 
     print("f1 score = {}, precision ={}, recall = {}".format(f1_score, precision, recall))
 
-def get_f1_score_precision_recall_six_inputs(model, dataloader):
-    truth = []
-    predections = []
-    with torch.no_grad():
-        for i, batch in enumerate(dataloader):
-            model.eval()
-            input_1 = batch["image"][0].to("cuda", dtype=torch.float)
-            input_2 = batch["image"][1].to("cuda", dtype=torch.float)
-            input_3 = batch["image"][2].to("cuda", dtype=torch.float)
-            input_4 = batch["image"][3].to("cuda", dtype=torch.float)
-            input_5 = batch["image"][4].to("cuda", dtype=torch.float)
-            input_6 = batch["image"][5].to("cuda", dtype=torch.float)
-        
-        
-            labels = batch["label"]
-            labels = np.asarray(labels)
-            labels = torch.from_numpy(labels.astype('long')).to("cuda")
 
-            outputs = model.to("cuda")(input_1, input_2, input_3, input_4, input_5, input_6)
-        
-            m = nn.Softmax()
-            softmax_output = m(outputs)       
-            #outputs = torch.column_stack((softmax_output[:,0] > 0.110333, softmax_output[:,1])).float()
-
-            if len(predections) == 0:
-                predections = softmax_output.cpu().detach().numpy()
-            else:
-                predections= np.concatenate((predections, softmax_output.cpu().detach().numpy()), axis=0)
-            
-            for k in range(labels.shape[0]):
-                truth.append(labels[k].item())
-
-    predicted_classes = np.argmax(predections, axis=1)
-    tp = 0
-    fn = 0
-    fp = 0
-    for i in range(len(truth)):
-
-        if (truth[i]==1 and predicted_classes[i] ==1):
-
-            tp+=1
-
-        elif (truth[i]==1 and predicted_classes[i] ==0):
-
-            fn+=1
-
-        elif (truth[i]==0 and predicted_classes[i] ==1):
-
-            fp+=1
-
-    precision = tp / (tp+fp)
-    recall = tp / (tp+fn)
-    f1_score = 2 * precision * recall / (precision + recall)
-
-    print("f1 score = {}, precision ={}, recall = {}".format(f1_score, precision, recall))
+#evaluation metrics for a single-inputs experiment
 
 def get_confusion_matrix_AUC_single_input(model, dataloader, axis, nb_classes=2):
     truth = []
@@ -412,7 +392,7 @@ def get_confusion_matrix_AUC_single_input(model, dataloader, axis, nb_classes=2)
 
     
 
-    y = label_binarize(truth, classes=[0,1,2])
+    y = label_binarize(truth, classes=[0,1])
 
 
 
